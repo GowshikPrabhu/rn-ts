01d57c1128a1e05fdf1823074801201968ab8abc
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-03 12:13:08 +0530

    Set a button in app

--- App.tsx
+++ App.tsx
@@ -8,39 +8,13 @@ export interface Props {
 }
 
 const App: React.FC<Props> = (props) => {
-  const [enthusiasmLevel, setEnthusiasmLevel] = React.useState(
-    props.enthusiasmLevel,
-  );
-
-  const onIncrement = () => setEnthusiasmLevel((enthusiasmLevel || 0) + 1);
-  const onDecrement = () => setEnthusiasmLevel((enthusiasmLevel || 0) - 1);
-
-  const getExclamationMarks = (numChars: number) =>
-    Array(numChars + 1).join('!');
   return (
     <View style={styles.root}>
-      <Text style={styles.greeting}>
-        Hello {props.name + getExclamationMarks(enthusiasmLevel || 0)}
-      </Text>
-
-      <View style={styles.buttons}>
-        <View style={styles.button}>
-          <Button
-            title="-"
-            onPress={onDecrement}
-            accessibilityLabel="decrement"
-            color="red"
-          />
-        </View>
-
-        <View style={styles.button}>
-          <Button
-            title="+"
-            onPress={onIncrement}
-            accessibilityLabel="increment"
-            color="blue"
-          />
-        </View>
+      <Text>Just a hello</Text>
+      <View style={styles.button}>
+        <Button title="toast" onPress={() => console.log('Clicked')}>
+          Toast
+        </Button>
       </View>
     </View>
   );
@@ -52,20 +26,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'center',
   },
-  buttons: {
-    flexDirection: 'row',
-    minHeight: 70,
-    alignItems: 'stretch',
-    alignSelf: 'center',
-    borderWidth: 5,
-  },
   button: {
     flex: 1,
-    paddingVertical: 0,
-  },
-  greeting: {
-    color: '#999',
-    fontWeight: 'bold',
+    justifyContent: 'center',
   },
 });
 
